304a2c65ddecbfbc609ff7c3d2b42f5c0d62d6e2
Nguyen Ngoc Duc <72563171+ndwannafly@users.noreply.github.com>
2020-12-04 16:28:41 +0300

    Update Main.java

--- lab3/src/main/java/ITMO_LAB3/Main.java
+++ lab3/src/main/java/ITMO_LAB3/Main.java
@@ -7,28 +7,28 @@ public class Main {
     public static void main(String[] args) {
         Driver bagel = new Driver("Bagel","unknown",Locations.GREENCITY,"car");
         Znayka znayka = new Znayka("Znayka","unknown",Locations.GREENCITY);
-        bagel.action();
-        bagel.volunteerAction("Znayka");
-        bagel.move(bagel.getLocation());
-        znayka.move(znayka.getLocation());
+        System.out.println(bagel.action());
+        System.out.println(bagel.volunteerAction("Znayka"));
+        System.out.println(bagel.move(bagel.getLocation()));
+        System.out.println(znayka.move(znayka.getLocation()));
         ArrayList<Resident> zmeevka = new ArrayList<Resident>();
         Resident representative = new Resident("residents","cheerful faces",Locations.ZMEEVKA,0,"Zmeevka");
-        representative.move(Locations.STREET);
-        representative.glad();
-        bagel.beFound();
+        System.out.println(representative.move(Locations.STREET));
+        System.out.println(representative.glad());
+        System.out.println(bagel.beFound());
         Screwdriver screwdriver = new Screwdriver("Screwdriver","unknown",Locations.ZMEEVKA);
         Gvozdyk gvozdyk = new Gvozdyk("Gvozdyk","unknown",Locations.RIVERSIDE, "panties");
-        gvozdyk.beReeducated();
+        System.out.println(gvozdyk.beReeducated());
         int number_of_residents = (int)(Math.random()*100)+1;
         for (int i = 0 ; i < number_of_residents; i++){
             double randnum = Math.random();
             randnum = (randnum < 0.5) ? 0 : 1;
             zmeevka.add(new Resident("resident" + Integer.toString(i), "cheerful faces", Locations.ZMEEVKA,(int) randnum, "Zmeevka"));
         }
-        Resident.countBeliever();
-        representative.lookAtSomeOne("Gvozdyk", "suspiciously");
-        representative.action();
-        gvozdyk.beFound();
-        gvozdyk.action();
+        System.out.println(Resident.countBeliever());
+        System.out.println(representative.lookAtSomeOne("Gvozdyk", "suspiciously"));
+        System.out.println(representative.action());
+        System.out.println(gvozdyk.beFound());
+        System.out.println(gvozdyk.action());
     }
 }
